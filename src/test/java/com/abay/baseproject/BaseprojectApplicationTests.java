416505b2ef84dcfa5fa4f834a16db3b86b21ee21
Abayneh <tabayneh@gmail.com>
2017-12-18 20:56:55 -0800

    i18N additions

--- src/test/java/com/abay/baseproject/BaseprojectApplicationTests.java
+++ src/test/java/com/abay/baseproject/BaseprojectApplicationTests.java
@@ -1,13 +1,30 @@
 package com.abay.baseproject;
 
+import com.abay.BaseProjectApplication;
+import com.abay.web.i18n.I18NService;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
-public class BaseprojectApplicationTests {
+@SpringBootTest(classes = BaseProjectApplication.class)
+@WebAppConfiguration
+public class BaseProjectApplicationTests {
+	@Autowired
+	private I18NService i18NService;
+
+	@Test
+	public void testMessageByLocaleService() throws Exception{
+		String expectedResult = "Bootstrap starter template";
+		String messageId = "index.main.callout";
+		String actual = i18NService.getMessage(messageId);
+		Assert.assertEquals("The actual and expected Strings don't match", expectedResult, actual);
+	}
 
 	@Test
 	public void contextLoads() {
